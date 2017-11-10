c09649e1b4d640643eb2df3a52b51f494c63c99b
Andrei Pinta <andrei.pinta@metrosystems.net>
2017-11-10 15:47:51 +0200

    NO-TICKET - Change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import logo from './logo.svg';
 import './App.css';
 import SpecialInputBox from './components/SpecialInputBox';
@@ -7,9 +8,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <MuiThemeProvider>
         <header className="App-header">
           <SpecialInputBox/>
         </header>
+      </MuiThemeProvider>
       </div>
     );
   }
--- src/components/SpecialInputBox.js
+++ src/components/SpecialInputBox.js
@@ -1,4 +1,7 @@
 import React,{Component} from 'react';
+import RaisedButton from 'material-ui/RaisedButton';
+import AppBar from 'material-ui/AppBar';
+import './SpecialInputBox.css';
 
 class SpecialInputBox extends Component {
 
@@ -6,27 +9,25 @@ class SpecialInputBox extends Component {
         super(props,context);
         this.onInputValueChange = this.onInputValueChange.bind(this);
         this.onLeavingInput = this.onLeavingInput.bind(this);
-        this.state = {inputValue:'', caption: '', captionWithName: ''};
+        this.state = {inputValue:'', isChanging: false};
+
     }
 
     onInputValueChange(event) {
-        this.setState({caption: '', captionWithName: ''});
-        this.setState({inputValue: event.target.value});
+        this.setState({inputValue: event.target.value, isChanging:true});
     }
 
     onLeavingInput(event) {
-        this.setState({caption: `Martzafoaico, somaldoaco! Afaaraaaa !! AAFFAAAARAA!`});
-        this.setState({captionWithName: `O sa trimit politia dupa tine, ${this.state.inputValue}!`});
-        console.log(this.state);
+        this.setState({isChanging:false});
     }
 
     render() {
         return (
-        <div>    
-        <p> this is a special input box. just type your name and you will be happy</p>
+        <div>
+         <AppBar title='The Reviews app' children={<h1>WOW</h1>} className='.appbar' />   
         <input value={this.state.inputValue} type='text' onChange={this.onInputValueChange} onBlur={this.onLeavingInput} />
-        <h1>{this.state.caption}</h1>    
-        <p>{this.state.captionWithName}</p>
+         <p>{this.state.isChanging ? this.state.inputValue : this.state.inputValue.toUpperCase() }</p>
+         <RaisedButton label="What the fuck?"  />
         </div>
         );
     }
